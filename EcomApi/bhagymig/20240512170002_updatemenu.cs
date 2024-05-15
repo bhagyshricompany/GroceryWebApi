18d34e6de0beecf11c4e63ab534dfedd90b4abbb
bhagyshricompany <116645353+bhagyshricompany@users.noreply.github.com>
2024-05-16 02:27:22 +0530

    ecomgroc

---
+++ EcomApi/bhagymig/20240512170002_updatemenu.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedMangoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "https://readmango.blob.core.windows.net/image/ad2.png");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "https://readmango.blob.core.windows.net/image/ad3.png");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "https://readmango.blob.core.windows.net/image/ad4.png");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "https://readmango.blob.core.windows.net/image/ad5.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/spring roll.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/idli.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/pani puri.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "https://redmangoimages.blob.core.windows.net/redmango/hakka noodles.jpg");
+        }
+    }
+}
